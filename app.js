279fc27f53adee884e93c4e92c727c745482dcd1
JD Gonzalez <jdg99@live.com>
2017-03-13 19:51:34 +0000

    just starting back up

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var routes = require('./app/routes.js');
+var mongoose = require("mongoose");
+
+var app = express();
+
+require('./app/config/passport')(passport);
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MONGO_URI);
+mongoose.set("debug", false);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+	secret: 'secretVotingApp',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+routes(app, passport);
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
