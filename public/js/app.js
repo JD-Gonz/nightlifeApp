fcf835cf0cc3c6c1948c9038865a4dc22dd1f04c
JD Gonzalez <jdg99@live.com>
2017-03-21 16:07:36 +0000

    Layout MVP complete

--- public/js/app.js
+++ public/js/app.js
@@ -4,46 +4,31 @@
 var app = angular.module("nightlifeApp", ["ui.router"]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  
-  
+
+
   $urlRouterProvider.otherwise('/');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/',
-            templateUrl: 'views/home.html'
-        })
-        
-        // nested list with custom controller
-        .state('home.list', {
-            url: '/list',
-            templateUrl: 'views/home-list.html',
-            controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
-        })
-        
-        // nested list with just some random string data
-        .state('home.paragraph', {
-            url: '/paragraph',
-            template: 'I could sure use a drink right now.'
-        })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            url: '/about',
-            views: {
-                '': { templateUrl: 'views/partial-about.html' },
-                'columnOne@about': { template: 'Look I am a column!' },
-                'columnTwo@about': { 
-                    templateUrl: 'views/table-data.html',
-                    controller: 'scotchController'
-                }
-            }
-            
-        });
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      views: {
+        '': { templateUrl: 'views/home.html' },
+        'search@home': { templateUrl: 'views/home-search.html' },
+        'list@home': { templateUrl: 'views/home-list.html' },
+        'footer@home': { templateUrl: 'views/footer.html' }
+      }
+    })
+    .state('home.search', {
+      url: 'search',
+      template: 'I could sure use a drink right now.'
+    })
+    .state('home.list', {
+      url: 'list',
+      templateUrl: 'views/home-list.html',
+      controller: function($scope) {
+        $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+      }
+    });
 });
 
 var checkLoggedin = function($q, $timeout, $http, $location, $rootScope) {
